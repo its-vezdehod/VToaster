2b1be57a147ffc197d616d37fc0bd91b45b02458
themestl <its.m.vezdehod@gmail.com>
2022-06-12 22:47:29 +0300

    prefer configs instead of hardcoded toasts

--- src/vezdehod/toaster/VToasterMain.php
+++ src/vezdehod/toaster/VToasterMain.php
@@ -7,7 +7,6 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\EventPriority;
 use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\resourcepacks\ResourcePack;
 use pocketmine\scheduler\ClosureTask;
@@ -15,23 +14,26 @@ use ReflectionClass;
 use Throwable;
 use vezdehod\toaster\pack\RuntimePackBuilder;
 use vezdehod\toaster\queue\GlobalToastQueue;
+use function array_keys;
 use function array_shift;
 use function implode;
 use function is_array;
+use function mb_substr;
+use function str_starts_with;
 use function strtolower;
 
 class VToasterMain extends PluginBase {
 
     private GlobalToastQueue $queue;
 
-    private Toast $default;
-    private Toast $error;
-    private Toast $warning;
+    /** @var array<string, Toast> */
+    private array $toasts = [];
 
     /**
      * @throws Exception
      */
     protected function onLoad(): void {
+        $this->saveResource('config.yml');
         $this->saveResource("toast-background-slice.json");
         $this->saveResource("toast-background-slice.png");
         $this->saveResource("error.ogg");
@@ -40,18 +42,31 @@ class VToasterMain extends PluginBase {
         $this->queue = new GlobalToastQueue();
         ToastFactory::setFactory(fn(ToastOptions $options) => new Toast($options->getSound()->getName(), $options->getFlag(), $this->queue));
 
-
-        $this->default = ToastFactory::create(ToastOptions::create($this, 'default')
-            ->defaultSound("random.toast")
-            ->defaultIcon("textures/ui/infobulb.png"));
-
-        $this->error = ToastFactory::create(ToastOptions::create($this, 'error')
-            ->fileIcon($this->getDataFolder() . "error.png")
-            ->fileSound($this->getDataFolder() . "error.ogg"));
-
-        $this->warning = ToastFactory::create(ToastOptions::create($this, 'warning')
-            ->downloadIcon("https://files.softicons.com/download/internet-icons/3d-ii-icons-by-la-glanz-studio/png/256/warning.png")
-            ->downloadSound("https://bigsoundbank.com/UPLOAD/ogg/2380.ogg"));
+        foreach ($this->getConfig()->get("toasts") as $toast => $rawOptions) {
+            $options = ToastOptions::create($this, $toast);
+            if (isset($rawOptions['icon'])) {
+                $icon = $rawOptions['icon'];
+                if (str_starts_with($icon, "./")) {
+                    $options->fileIcon($this->getDataFolder() . mb_substr($icon, 2));
+                } else if (str_starts_with($icon, "http")) {
+                    $options->downloadIcon($icon);
+                } else {
+                    $options->defaultIcon($icon);
+                }
+            }
+            if (isset($rawOptions['sound'])) {
+                $sound = $rawOptions['sound'];
+                if (str_starts_with($sound, "./")) {
+                    $options->fileSound($this->getDataFolder() . mb_substr($sound, 2));
+                } else if (str_starts_with($sound, "http")) {
+                    $options->downloadSound($sound);
+                } else {
+                    $options->defaultSound($sound);
+                }
+            }
+            //TODO: Animations, background
+            $this->toasts[$toast] = ToastFactory::create($options);
+        }
     }
 
     protected function onEnable(): void {
@@ -81,21 +96,24 @@ class VToasterMain extends PluginBase {
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        if (!($sender instanceof Player)) {
-            return true;
-        }
         if (count($args) < 3) {
             return false;
         }
 
-        $toast = match (array_shift($args)) {
-            "error" => $this->error,
-            "warning" => $this->warning,
-            default => $this->default
-        };
+        $target = $this->getServer()->getPlayerByPrefix(array_shift($args));
+        if ($target === null) {
+            $sender->sendMessage("Player not found!");
+            return true;
+        }
+
+        $toast = $this->toasts[array_shift($args)] ?? null;
+        if ($toast === null) {
+            $sender->sendMessage("Toast not found! Available toasts: " . implode(", ", array_keys($this->toasts)));
+            return true;
+        }
 
 
-        $header = null;
+        $message = null;
         $silent = false;
         $immediately = false;
         foreach ($args as $i => $arg) {
@@ -105,23 +123,24 @@ class VToasterMain extends PluginBase {
             } else if ($arg === "-n") {
                 $immediately = true;
                 unset($args[$i]);
-            } else if ($arg === "-h") {
-                $header = [];
+            } else if ($arg === "-m") {
+                $message = [];
                 unset($args[$i]);
-            } else if (is_array($header)) {
-                $header[] = $arg;
+            } else if (is_array($message)) {
+                $message[] = $arg;
                 unset($args[$i]);
             }
         }
 
-        $message = implode(" ", $args);
-        $header = $header === null || count($header) === 0 ? null : implode(" ", $header);
+        $header = implode(" ", $args);
+        $message = $message === null || count($message) === 0 ? null : implode(" ", $message);
         match (true) {
-            $silent && $immediately => $toast->sendSilent($sender, $message, $header),
-            $silent && !$immediately => $toast->enqueueSilent($sender, $message, $header),
-            !$silent && $immediately => $toast->send($sender, $message, $header),
-            !$silent && !$immediately => $toast->enqueue($sender, $message, $header)
+            $silent && $immediately => $toast->sendSilent($target, $header, $message),
+            $silent && !$immediately => $toast->enqueueSilent($target, $header, $message),
+            !$silent && $immediately => $toast->send($target, $header, $message),
+            !$silent && !$immediately => $toast->enqueue($target, $header, $message)
         };
+        $sender->sendMessage("Toast successfully " . ($immediately ? "sent" : "enqueued")  . " to {$target->getName()} " . ($silent ? "(without sound)" : ""));
         return true;
     }
 
